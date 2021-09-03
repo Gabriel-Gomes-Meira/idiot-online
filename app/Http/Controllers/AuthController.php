af4e575510e8b843f366c36657d155b57352c275
Shuaybgol <62515106+Shuaybgol@users.noreply.github.com>
2021-09-02 22:43:08 -0300

    Revert "Problema no JWT"
    This reverts commit efd466bcf4c218c8a7a1923580cd21f5137ab6ad.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,132 +2,60 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\Controller;
-// use Illuminate\Http\Request;
-// use App\Models\Card;
-// use App\Models\User;
-// use Illuminate\Support\Facades\DB;
+use App\Models\Card;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
-    // Dashboard do back-end está pra ser passado pro front... Algum dia...
-    // public function adminv()
-    // {
-    //     if(Auth::check() === true){
-    //         return view('admin.dashboard');
-    //     }
-
-    //     return view('admin.login');
-    // }
-
-    // public function login(Request $request)
-    // {
-    //     $credenciais = [
-    //         "email" => $request->email,
-    //         "password" =>$request->password,
-    //         "admin" => 1
-    //     ];
-
-    //     if(Auth::attempt($credenciais)){
-    //         return view('admin.dashboard');
-    //     }
-
-    //     return redirect()->back()->withInput()->withErrors(['Dados inválidos!!']);
-
-    // }
-
-    // public function baralho()
-    // {
-    //     $cards = Card::all();
-    //     return view('admin.dashboard.cards', compact('cards'));
-    // }
-
-    // public function rooms()
-    // {
-    //     $Rooms = DB::select(
-    //     'Select r.*, u.name as hoster, u2.name as guester, uw.name as winner from rooms r
-    //         inner join users u on (u.id = r.player1)
-    //         left join users u2 on(u2.id = r.player2)
-    //         left join users uw on (uw.id = r.winner)
-    //         order by r.id desc');
-    //     return view('admin.dashboard.rooms', compact('Rooms'));
-    // }
-
-    // public function users()
-    // {
-    //     $Users = User::where('admin', false)->get();
-    //     $Users = collect($Users)->except('admin');
-    //     return view('admin.dashboard.users', compact('Users'));
-    // }
-
-    public function __construct()
+    public function adminv()
     {
-        $this->middleware('auth:api', ['except' => ['login']]);
-    }
-
-    /**
-     * Get a JWT via given credentials.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function login()
-    {
-        $credentials = request(['email', 'password']);
-
-        if (! $token = Auth::attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+        if(Auth::check() === true){
+            return view('admin.dashboard');
         }
 
-        return $this->respondWithToken($token);
+        return view('admin.login');
     }
 
-    /**
-     * Get the authenticated User.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function me()
+    public function login(Request $request)
     {
-        return response()->json(Auth::user());
+        $credenciais = [
+            "email" => $request->email,
+            "password" =>$request->password,
+            "admin" => 1
+        ];
+
+        if(Auth::attempt($credenciais)){
+            return view('admin.dashboard');
+        }
+
+        return redirect()->back()->withInput()->withErrors(['Dados inválidos!!']);
+
     }
 
-    /**
-     * Log the user out (Invalidate the token).
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function logout()
+    public function baralho()
     {
-        Auth::logout();
-
-
-        return response()->json(['message' => 'Successfully logged out']);
+        $cards = Card::all();
+        return view('admin.dashboard.cards', compact('cards'));
     }
 
-    /**
-     * Refresh a token.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function refresh()
+    public function rooms()
     {
-        return $this->respondWithToken(Auth::refresh());
+        $Rooms = DB::select(
+        'Select r.*, u.name as hoster, u2.name as guester, uw.name as winner from rooms r
+            inner join users u on (u.id = r.player1)
+            left join users u2 on(u2.id = r.player2)
+            left join users uw on (uw.id = r.winner)
+            order by r.id desc');
+        return view('admin.dashboard.rooms', compact('Rooms'));
     }
 
-    /**
-     * Get the token array structure.
-     *
-     * @param  string $token
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    protected function respondWithToken($token)
+    public function users()
     {
-        return response()->json([
-            'access_token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => Auth::factory()->getTTL() * 60
-        ], 200);
+        $Users = User::where('admin', false)->get();
+        $Users = collect($Users)->except('admin');
+        return view('admin.dashboard.users', compact('Users'));
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,16 +2,15 @@
 
 namespace App\Models;
 
-// use Laravel\Sanctum\HasApiTokens;
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -45,24 +44,4 @@ class User extends Authenticatable implements JWTSubject
     protected $hidden = [
         'password'
     ];
-
-    /**
-     * Get the identifier that will be stored in the subject claim of the JWT.
-     *
-     * @return mixed
-     */
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    /**
-     * Return a key value array, containing any custom claims to be added to the JWT.
-     *
-     * @return array
-     */
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
 }
